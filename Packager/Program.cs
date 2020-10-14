417425edae20fc8033c953d91baf0b5199c5203f
dlebansais <dlebansais@gmail.com>
2020-10-14 20:39:29 +0200

    Add project files.

---
+++ Packager/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Packager
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] arguments)
+        {
+            string CurrentDirectory = Environment.CurrentDirectory;
+            Console.WriteLine($"Current Directory: {CurrentDirectory}");
+        }
+    }
+}
