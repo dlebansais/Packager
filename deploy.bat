764ff21394d240cdac45744ae4907c43e2f4ae37
dlebansais <dlebansais@gmail.com>
2020-10-20 20:49:53 +0200

    CI update.

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,5 @@
 @echo off
-if '%1' == '' goto error
-echo Deploying %1 ...
-git commit --allow-empty -m "%1"
+echo Deploying ...
 git fetch . master:deployment
 git push origin deployment
-goto end
-
-:error
-echo Parameter: version tag. Ex: deploy v1.0.0
-goto end
-
 :end
