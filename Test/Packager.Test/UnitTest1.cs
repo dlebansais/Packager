0d56a2d328e8cc358fb7f9d432741bf2ec7a2361
David Le Bansais <dlebansais@gmail.com>
2024-09-15 21:36:08 +0200

    Added tests.

--- Test/Packager.Test/UnitTest1.cs
+++ Test/Packager.Test/UnitTest1.cs
@@ -12,7 +12,7 @@ public class UnitTest1
     [Test]
     public void TestNoParameter()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName);
+        bool IsSuccessful = Launcher.Launch(TestedAppName);
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -20,7 +20,7 @@ public class UnitTest1
     [Test]
     public void TestSampleSolution()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments:null, workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments:null, workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -28,7 +28,7 @@ public class UnitTest1
     [Test]
     public void TestDebug()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--debug", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--debug", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -36,7 +36,7 @@ public class UnitTest1
     [Test]
     public void TestInvalidSolution()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-001");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-001");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -44,7 +44,7 @@ public class UnitTest1
     [Test]
     public void TestConditionalRelease()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -52,7 +52,7 @@ public class UnitTest1
     [Test]
     public void TestConditionalDebug()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--debug", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--debug", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -60,7 +60,7 @@ public class UnitTest1
     [Test]
     public void TestMissingInfo()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-002");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-002");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -68,7 +68,7 @@ public class UnitTest1
     [Test]
     public void TestMerge()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -76,7 +76,7 @@ public class UnitTest1
     [Test]
     public void TestMergeWithError()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-003");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-003");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -84,7 +84,7 @@ public class UnitTest1
     [Test]
     public void TestMergeValidName()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge:Helper", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge:Helper", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -92,7 +92,7 @@ public class UnitTest1
     [Test]
     public void TestMergeInvalidName()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge:Invalid", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge:Invalid", workingDirectory: @"TestSolutions\Method.Contracts.Analyzers");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -100,7 +100,7 @@ public class UnitTest1
     [Test]
     public void TestMergeWithNoSolution()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-004");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-004");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -108,7 +108,7 @@ public class UnitTest1
     [Test]
     public void TestMergeWithNoProjects()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-005");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-005");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -116,7 +116,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedVersions()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-006");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-006");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -124,7 +124,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedAuthor()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-007");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-007");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -132,7 +132,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedCopyright()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-008");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-008");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -140,7 +140,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedRepositoryUrk()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-009");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-009");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -148,7 +148,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedFrameworks1()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-1");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-1");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -156,7 +156,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedFrameworks2()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-2");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-2");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -164,7 +164,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedFrameworks3()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-3");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-3");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -172,7 +172,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedFrameworks4()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-4");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-010-4");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -180,7 +180,7 @@ public class UnitTest1
     [Test]
     public void TestDescription()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--description \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--description \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -188,7 +188,7 @@ public class UnitTest1
     [Test]
     public void TestMergeAndDescription()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge --description \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge --description \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -196,7 +196,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedDependencyVersion()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-011");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-011");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -204,7 +204,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedDependencyVersionTwo()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-012");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-012");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -212,7 +212,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedDependencyVersionMultiple1()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-1");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-1");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -220,7 +220,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedDependencyVersionMultiple2()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-2");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-2");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -228,7 +228,7 @@ public class UnitTest1
     [Test]
     public void TestMergeMixedDependencyVersionMultiple3()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-3");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--merge", workingDirectory: @"TestSolutions\Invalid-013-3");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -236,7 +236,7 @@ public class UnitTest1
     [Test]
     public void TestAnalyzer()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--analyzer", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--analyzer", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -244,7 +244,7 @@ public class UnitTest1
     [Test]
     public void TestDebugAnalyzer()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--debug --analyzer", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--debug --analyzer", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -252,7 +252,7 @@ public class UnitTest1
     [Test]
     public void TestNuspecPrefix()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--prefix \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--prefix \"test\"", workingDirectory: @"TestSolutions\Method.Contracts");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -277,7 +277,7 @@ public class UnitTest1
     {
         BackupPgSearchIcon();
 
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\PgSearch");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\PgSearch");
 
         RestorePgSearchIcon();
 
@@ -289,7 +289,7 @@ public class UnitTest1
     {
         BackupPgSearchIcon();
 
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--icon \"Resources\\main.ico\"", workingDirectory: @"TestSolutions\PgSearch");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--icon \"Resources\\main.ico\"", workingDirectory: @"TestSolutions\PgSearch");
 
         RestorePgSearchIcon();
 
@@ -301,7 +301,7 @@ public class UnitTest1
     {
         BackupPgSearchIcon();
 
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: "--icon \"Resources\\main.png\"", workingDirectory: @"TestSolutions\PgSearch");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: "--icon \"Resources\\main.png\"", workingDirectory: @"TestSolutions\PgSearch");
 
         RestorePgSearchIcon();
 
@@ -311,7 +311,7 @@ public class UnitTest1
     [Test]
     public void TestMissingDetails()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments:null, workingDirectory: @"TestSolutions\Invalid-014");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments:null, workingDirectory: @"TestSolutions\Invalid-014");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -319,7 +319,7 @@ public class UnitTest1
     [Test]
     public void TestNoDependency()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-015");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-015");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -327,7 +327,7 @@ public class UnitTest1
     [Test]
     public void TestOtherFrameworks()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-016");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-016");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -335,7 +335,7 @@ public class UnitTest1
     [Test]
     public void TestMissingIcon()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-017");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-017");
 
         Assert.That(IsSuccessful, Is.True);
     }
@@ -343,7 +343,23 @@ public class UnitTest1
     [Test]
     public void TestMissingPng()
     {
-        bool IsSuccessful =Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-018");
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-018");
+
+        Assert.That(IsSuccessful, Is.True);
+    }
+
+    [Test]
+    public void TestNotPackable()
+    {
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-019");
+
+        Assert.That(IsSuccessful, Is.True);
+    }
+
+    [Test]
+    public void TestIsTestProject()
+    {
+        bool IsSuccessful = Launcher.Launch(TestedAppName, arguments: null, workingDirectory: @"TestSolutions\Invalid-020");
 
         Assert.That(IsSuccessful, Is.True);
     }
